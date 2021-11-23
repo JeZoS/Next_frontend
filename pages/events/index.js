a6e7ba6c1865c048a32a793ab886ee964bd368ef
JeZoS <prabhat262111@gmail.com>
2021-11-23 11:12:09 +0530

    added simple api

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,26 @@
-import Layout from "../../components/Layout";
+import EventsItem from "@/components/EventsItem";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
-    return (
-        <Layout>
-            <h1>My Events</h1>
-        </Layout>
-    )
+export default function EventsPage({ events }) {
+  return (
+    <Layout>
+      <h1>Events</h1>
+      {events.length == 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventsItem key={evt.id} evt={evt} />
+      ))}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const resp = await fetch(`${API_URL}/api/events`);
+  const events = await resp.json();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 1,
+  };
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import EventsItem from "@/components/EventsItem";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-     
+      <h1>Upcoming Events</h1>
+      {events.length == 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventsItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const resp = await fetch(`${API_URL}/api/events`);
+  const events = await resp.json();
+  return {
+    props: {
+      events: events.slice(0, 3),
+    },
+    revalidate: 1,
+  };
 }
